68188a0d5b23da9749a30ae06916f8709f91df6a
shreyansh0297 <s.limbachia.r@gmail.com>
2021-12-27 15:51:03 +0530

    next commit

--- zc-frontend/src/app/app-routing.module.ts
+++ zc-frontend/src/app/app-routing.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './signinout/login/login/login.component';
 import { RegisterComponent } from './signinout/register/register/register.component';
+import { TraderDashboardComponent } from './trader/trader-dashboard/trader-dashboard/trader-dashboard.component';
 import { WelcomeComponent } from './welcome/welcome/welcome.component';
 
 const routes: Routes = [
   { path: 'welcome', component: WelcomeComponent },
   { path: 'register', component: RegisterComponent},
   { path: 'login', component: LoginComponent},
-
+  { path:'traderdashboard', component: TraderDashboardComponent },
   { path: '', pathMatch: 'full', redirectTo: 'welcome'},
 ];
 
